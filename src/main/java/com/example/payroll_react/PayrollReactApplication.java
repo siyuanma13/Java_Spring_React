1a0151a4d8421a67b4362c562e8a958b1ac7fe8d
Siyuan Ma <siyuanma@Siyuans-MacBook-Air.local>
2022-03-26 11:23:53 -0500

    initial commit

---
+++ src/main/java/com/example/payroll_react/PayrollReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.payroll_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/payroll_react/PayrollReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payroll_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
