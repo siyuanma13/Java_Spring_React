8f2696f95426d16e5e9736ba9f591a8ea40e840f
Siyuan Ma <siyuanma@Siyuans-MacBook-Air.local>
2022-03-30 17:53:56 -0400

    added custom get in repository

--- src/main/java/com/example/payroll_react/Repositories/EmployeeRepository.java
+++ src/main/java/com/example/payroll_react/Repositories/EmployeeRepository.java
@@ -2,6 +2,12 @@ package com.example.payroll_react.Repositories;
 
 import com.example.payroll_react.Data.Employee;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
+//@RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+    Employee findByLastName(@Param("lastName") String lastName);
 }
